94c321f6b80fa30b0b0dfbb720e7f4bf97472e29
tiffany <tiffany831101@gmail.com>
2018-12-01 01:41:55 +0800

    Updates

---
+++ precache-manifest.6133f0301e0760c248a44bc43ee5fbc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "8ae528924c92c06adcef",
+    "url": "/my-app/static/js/main.8ae52892.chunk.js"
+  },
+  {
+    "revision": "9895c1ec9258a0bb6164",
+    "url": "/my-app/static/js/1.9895c1ec.chunk.js"
+  },
+  {
+    "revision": "8ae528924c92c06adcef",
+    "url": "/my-app/static/css/main.fbd1e18c.chunk.css"
+  },
+  {
+    "revision": "0ebc21c89500fa2cbd12c18030c397fc",
+    "url": "/my-app/index.html"
+  }
+];
